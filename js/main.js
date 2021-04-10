738e84daf86b9a4d6f88564b3a5f26ff968c3836
Sehab-sketch <abdullahalsehab2017@gmail.com>
2021-04-10 21:13:12 +0600

    try to fix mobile extra whide problem

--- js/main.js
+++ js/main.js
@@ -2,4 +2,8 @@
 
   $(document).ready(function(){
     $('.slider_area').bxSlider();
+    $('#main_menu').meanmenu({
+      meanmenuContainer: 'mob_nav',
+      meanScreenWidth: 991,
+    });
   });
